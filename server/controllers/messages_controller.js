66de2d63594d35e1a3c45d18a005a0e6c8e4ccd5
Katie <adlerk8@gmail.com>
2020-10-27 14:26:03 -0700

    ready for review

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,11 +1,11 @@
 let messages = [];
-
 let id = 0;
 
 module.exports = {
     create: (req, res) => {
         const {text, time} = req.body;
         const message = {
+            id,
             text, 
             time
         }
@@ -17,10 +17,18 @@ module.exports = {
         res.status(200).send(messages);
     },
     update: (req, res) => {
-        
+        const {text} = req.body;
+        let index = messages.findIndex(elem => elem.id === +req.params.id);
+        messages[index] = {
+            id: messages[index].id,
+            text: text || req.body.text,
+            time: messages[index].time
+        }
         res.status(200).send(messages);
     },
     delete: (req, res) => {
+        let index = messages.findIndex(elem => elem.id === +req.params.id);
+        messages.splice(index, 1);
         res.status(200).send(messages);
     }
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express;
+const app = express();
+const mc = require('../server/controllers/messages_controller')
+
 app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
+
+app.get('/api/messages', mc.read);
+
+app.post('/api/messages', mc.create)
+
+app.put('/api/messages/:id', mc.update);
+
+app.delete('/api/messages/:id', mc.delete);
 
 const port = 3001;
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
